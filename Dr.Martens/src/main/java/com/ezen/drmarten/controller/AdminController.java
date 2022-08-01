22fb28a9d594be5c06019fb27a5c965824c4d081
user <user@192.168.219.118>
2022-08-01 15:09:22 +0900

    푸시 확인

---
+++ Dr.Martens/src/main/java/com/ezen/drmarten/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.ezen.drmarten.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ezen.drmarten.mappers.BoardMapper;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	private BoardMapper dao;
+
+	@GetMapping("")
+	@ResponseBody
+	public String loginForm() {
+		return "thymeleaf/admin/login";
+	}
+
+	
+}
